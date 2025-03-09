4670ed7f1779eec2c56470ede46365a12e97e158
TharHtet236401 <tharhteta3@gmail.com>
2025-03-09 19:04:24 +0700

    showing record

--- dcrmApp/views.py
+++ dcrmApp/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
-
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -18,7 +19,7 @@ def home(request):
         else:
             messages.error(request, "Invalid username or password")
             return redirect('home')
-    return render(request, 'home.html',{})
+    return render(request, 'home.html',{'records':records})
 
 def register_user(request):
     if request.method == 'POST':
