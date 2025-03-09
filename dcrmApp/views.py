9f3e37da0890c441b01bcff56f375c92c991938a
TharHtet236401 <tharhteta3@gmail.com>
2025-03-09 20:06:31 +0700

    add the records

--- dcrmApp/views.py
+++ dcrmApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -57,6 +57,18 @@ def delete_record(request, pk):
         return redirect('home')
     else:
         messages.error(request, "You must be logged in to delete a record")
+        return redirect('home')
+
+def add_record(request):
+    if request.user.is_authenticated:
+        form = AddRecordForm(request.POST or None)
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                return redirect('home')
+        return render(request, 'add_record.html',{'form':form})
+    else:
+        messages.error(request, "You must be logged in to add a record")
         return redirect('home')
 
 def logout_user(request):
