2c7aceca9f19f44777ee12fa99aa338b7358d696
TharHtet236401 <tharhteta3@gmail.com>
2025-03-09 20:12:30 +0700

    add update feature

--- dcrmApp/views.py
+++ dcrmApp/views.py
@@ -71,6 +71,19 @@ def add_record(request):
         messages.error(request, "You must be logged in to add a record")
         return redirect('home')
 
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=record)
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                return redirect('home')
+        return render(request, 'update_record.html',{'form':form})
+    else:
+        messages.error(request, "You must be logged in to update a record")
+        return redirect('home')
+
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out!")
